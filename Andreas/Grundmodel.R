d5edaf37ced4f15f78b16c69788518bd86b77afa
Graverz <50443367+Graverz@users.noreply.github.com>
2019-05-09 11:41:01 +0200

    opd

--- Andreas/Grundmodel.R
+++ Andreas/Grundmodel.R
@@ -26,13 +26,47 @@ COM <- Quandl("") %>%
   filter(Date >= "1959-01-01", Date < "2019-01-01") %>% 
   arrange(Date)
 
-UNE <- Quandl("") %>% 
+UNE <- Quandl("FRED/UNRATE") %>% 
+  filter(Date >= "1959-01-01", Date < "2019-01-01") %>% 
+  arrange(Date)
+
+BOND <- Quandl("FED/SVENY", collapse = "m") %>% 
   filter(Date >= "1959-01-01", Date < "2019-01-01") %>% 
   arrange(Date)
 
 # Variabel  --------------------------------------------------------------
 
+FFR <- FFR$Value
+CPI <- CPI$Value
+PROD <- PROD$Value
+DOW <- Aktie$`US DOW JONES INDUSTRIALS SHARE PRICE INDEX (EP) NADJ`
+COM <-
+UNE <- UNE$Value
+SPREAD <- BOND$SVENY05-BOND$SVENY02
+SPREAD2 <- c(rnorm(29, mean = 0.4,sd = 0.1),SPREAD)
+  
+# Stationæritet-test  ----------------------------------------------------
 
-# Datasæt   --------------------------------------------------------------
+adf.test(SPREAD2)
+
+dFFR <- diff(FFR)
+dCPI <- diff(log(CPI))
+dPROD <- diff(log(PROD))
+dDOW <- diff(log(DOW))
+dCOM <- diff(log())
+dUNE <- diff(UNE)
+
+# Datasæt til model-------------------------------------------------------
+
+Data <-  cbind(dCPI,dPROD,dFFR)
+Data1 <- cbind(dCPI,dPROD,dFFR)
+Data2 <- cbind(dCPI,dPROD,dFFR)
+Data3 <- cbind(dCPI,dPROD,dFFR)
+Data4 <- cbind(dCPI,dPROD,dFFR)
 
 # Modeller  --------------------------------------------------------------
+
+V <- VAR(Data, p=)
+VARselect(V)
+irf()
+plot()
